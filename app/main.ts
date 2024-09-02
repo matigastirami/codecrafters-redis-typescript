55d4780873e694ddd636afe8fede32950df3f174
Matias Ramirez <mramirez@Matiass-MacBook-Pro.local>
2024-09-01 22:38:59 -0300

    chore: respond to ping

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  connection.write(Buffer.from("+PONG\r\n", 'utf-8'));
 });
 
 server.listen(6379, "127.0.0.1");
